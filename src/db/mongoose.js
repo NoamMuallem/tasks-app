60322e5a89acfb76cf6cf48db4398d68d92adb25
Noam Muallem <noammu072@gmail.com>
2020-01-11 20:14:00 +0200

    adding validation and sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+//external validator librery, mutch more extensive then mongoose costum validation
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/tasks-manager-api", {
   useNewUrlParser: true,
@@ -8,26 +10,63 @@ mongoose.connect("mongodb://127.0.0.1:27017/tasks-manager-api", {
 
 const User = mongoose.model("User", {
   name: {
-    type: String
+    type: String,
+    //adding validation and sanatization
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true, //removing spaces before and after content
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("this email is unvalid");
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    defalt: 0,
+    //costum validation, cant input a negative age
+    validate(value) {
+      if (value < 0) {
+        throw new Error("age must be a positive number");
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: [7, "password is to short"],
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("invalid password");
+      }
+    }
   }
 });
 
 const Task = mongoose.model("Task", {
   description: {
-    type: String
+    type: String,
+    trim: true,
+    required: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   }
 });
 
-// //creat a new user
+// //creat a new user, chacking validation
 // const me = new User({
-//   name: "Noam Muallem",
-//   age: 27
+//   name: "        Noam   ",
+//   age: 80,
+//   email: "MYEMAIL@git.il                             ",
+//   password: "PASSWORD"
 // });
 
 // me.save()
@@ -40,8 +79,7 @@ const Task = mongoose.model("Task", {
 
 //creat new task
 const task = new Task({
-  description: "do homework",
-  completed: false
+  description: "make plains"
 });
 
 task
