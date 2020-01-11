f600a608905771a4a3775478759d803e4ccf626d
Noam Muallem <noammu072@gmail.com>
2020-01-11 13:50:00 +0200

    practicing updating daata and USING PROMIESES

--- mongodb.js
+++ mongodb.js
@@ -21,51 +21,48 @@ MongoClient.connect(
     //creating new database, by trying to access it, it will be created for us
     const db = client.db(databaseName);
 
-    // //will fetch a user from database with query
-    // //with the callback function we can get error or the document itself
-    // db.collection("users").findOne({ name: "Noam Muallem" }, (error, user) => {
-    //   if (error) {
-    //     return "unable to fined requested document";
-    //   }
-
-    //   console.log(user);
-    // });
-
-    // //no match is not an error but null!!!
-    // db.collection("users").findOne({ name: "bob" }, (error, user) => {
-    //   if (error) {
-    //     return "unable to fined requested document";
-    //   }
-
-    //   console.log(user);
-    // });
-
-    // //to use a document id we need to pass an id OBJECT if not, we will get null
-    // db.collection("users").findOne(
-    //   { _id: new ObjectID("5e1991faf9dbf00833a576e5") },
-    //   (error, user) => {
-    //     if (error) {
-    //       return "unable to fined requested document";
-    //     }
-
-    //     console.log(user);
-    //   }
-    // );
-
-    //find return a currsor- pointer in the database to all the collections we fined
-    //this gives us more flexability with the output
-    //by usin toArray we can construct an arry from the currsore we got from fined
     db.collection("users")
-      .find({ age: 27 })
-      .toArray((error, users) => {
-        console.log(users);
+      .updateOne(
+        {
+          _id: new ObjectID("5e19997be416bfa88b3c08b7")
+        },
+        {
+          //change the fileds in the document
+          $set: {
+            name: "bob"
+          },
+          //incrimating by 1
+          $inc: {
+            age: 1
+          }
+        }
+      )
+      .then(result => {
+        ///promies fulfiled
+        console.log(result);
+      })
+      .catch(error => {
+        //promies rejected
+        console.log(error);
       });
 
-    //lets see hoe mutch maches can we get
-    db.collection("users")
-      .find({ age: 27 })
-      .count((error, count) => {
-        console.log(count);
+    //challenge: complite all the goals:
+    db.collection("tasks")
+      .updateMany(
+        {
+          completed: false
+        },
+        {
+          $set: {
+            completed: true
+          }
+        }
+      )
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
       });
   }
 );
