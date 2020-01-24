ae34a7d3700f236711080ea99480fa32e8a55501
Noam Muallem <noammu072@gmail.com>
2020-01-24 09:31:16 +0200

    creating end points to read users and user by id also creating endpoints for tasks and task by id

--- src/index.js
+++ src/index.js
@@ -9,6 +9,66 @@ const port = process.env.PORT || 3000;
 //automaticly pars incoming json
 app.use(express.json());
 
+//get all the tasks
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then(Tasks => {
+      res.send(Tasks);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+//get task by id
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then(task => {
+      if (!task) {
+        return res.status(404).send();
+      }
+
+      res.send(task);
+    })
+    .catch(e => {
+      res.status(500);
+    });
+});
+
+//get all the users on the database
+app.get("/users", (req, res) => {
+  //just fetch all the user, no search ceriteria
+  User.find({})
+    .then(Users => {
+      res.send(Users);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+//getting user with specific user id
+app.get("/users/:id", (req, res) => {
+  //:id a dinemic key (a tool provided by express)
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then(user => {
+      //is database is successfuly accesed and no user with that id was found
+      if (!user) {
+        return res.status(404).send();
+      }
+
+      //if a user was found with that id
+      res.send(user);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
 //creat task
 app.post("/task", (req, res) => {
   const task = new Task(req.body);
